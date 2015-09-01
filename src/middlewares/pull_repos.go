9676041e08bf6e2aae2c4b446e18ea4e0822e548
youngsterxyf <sas.198708@gmail.com>
2015-09-02 01:25:36 +0800

    fix bug

--- src/middlewares/pull_repos.go
+++ src/middlewares/pull_repos.go
@@ -69,10 +69,20 @@ func (pr PullReposStruct) Run(chanElement models.ChanElementStruct) bool {
         }
     }
 
-    // 拉取所有分支
     if isNew == false {
+        // 先切换到master分支，才能执行git pull -p
+        changeBranchCMD := exec.Command("git", "checkout", "master")
+        output, err := changeBranchCMD.Output()
+        if err != nil {
+            errMsg := fmt.Sprintf("%s; %s", string(output), err.Error())
+            chanElement.Mh.UpdateLogStatus(chanElement.HookID, "failure", errMsg)
+            fmt.Println(errMsg)
+            return false
+        }
+
+        // 拉取所有分支
         pullCMD := exec.Command("git", "pull", "-p")
-        output, err := pullCMD.Output()
+        output, err = pullCMD.Output()
         if err != nil {
             errMsg := fmt.Sprintf("%s; %s", string(output), err.Error())
             chanElement.Mh.UpdateLogStatus(chanElement.HookID, "failure", errMsg)
@@ -81,6 +91,7 @@ func (pr PullReposStruct) Run(chanElement models.ChanElementStruct) bool {
         }
     }
 
+    // 切换到目标分支
     changeBranchCMD := exec.Command("git", "checkout", chanElement.BranchName)
     output, err := changeBranchCMD.Output()
     if err != nil {
@@ -91,6 +102,7 @@ func (pr PullReposStruct) Run(chanElement models.ChanElementStruct) bool {
     }
 
     if isNew == false {
+        // 定位目标commit
         resetHardCMD := exec.Command("git", "reset", "--hard", chanElement.LatestCommit)
         output, err = resetHardCMD.Output()
         if err != nil {
